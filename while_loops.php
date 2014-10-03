96f1f051bcf35f175ca6013108f6597b46b67d42
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-03 09:19:18 +0200

    While en Do While lussen

--- while_loops.php
+++ while_loops.php
@@ -4,18 +4,29 @@
 		<title>while loops</title>
 	</head>
 	<body>
-		<h3>While Loops</h3>
+		<h3>While Loop</h3>
 		
 		<?php
-			$i = 6;
-			
-			while ($i >= 0)
+			$i = 6;			
+			while ($i > 0)
 			{
 				echo '<h'.$i.'>De waarde van $i is: '.$i.'</h'.$i.'>';
 				//$i = $i + 1;
 				//$i += 1;
 				$i--;
 			}
+		?>
+		<hr>
+		<h3>Do While Loop</h3>
+		<?php
+			$i = 6;
+			do
+			{
+				echo '<h'.$i.'>De waarde van $i is: '.$i.'</h'.$i.'>';
+				$i--;
+			
+			}while($i > 0)
+		
 		?>
 	</body>
 </html>
