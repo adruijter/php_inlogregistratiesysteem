41bf7462192f209de0bf4428d28663d641763c73
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-17 10:56:31 +0100

    Method showInfoAuto

--- AutoClass.php
+++ AutoClass.php
@@ -10,13 +10,40 @@
 		private $bouwjaar;
 		
 		// Properties worden get en set functies
-		public function getMerk() {	return $this->merk;	}
+		public function getMerk() 			{	return $this->merk;	}
+		public function getType() 			{	return $this->type;	}
+		public function getAantalWielen() 	{	return $this->aantalWielen;	}
+		public function getBrandstof() 		{	return $this->brandstof;	}
+		public function getKenteken() 		{	return $this->kenteken;	}
+		public function getBouwjaar() 		{	return $this->bouwjaar;	}
+		
 		
 		
 		// Constructor
-		public function __construct($merk)
+		public function __construct($merk, 
+									$type, 
+									$aantalWielen, 
+									$brandstof,
+									$kenteken,
+									$bouwjaar)
 		{
 			$this->merk = $merk;
-		}		
+			$this->type = $type;
+			$this->aantalWielen = $aantalWielen;
+			$this->brandstof = $brandstof;
+			$this->kenteken = $kenteken;
+			$this->bouwjaar = $bouwjaar;
+		}
+		
+		public function showInfoAuto()
+		{
+			$text  = "Deze auto is van het merk ".$this->merk."<br>";
+			$text .= "Het type is: ".$this->type."<br>";
+			$text .= "Deze auto heeft ".$this->aantalWielen." wielen<br>";
+			$text .= "Hij rijdt op: ".$this->brandstof."<br>";
+			$text .= "Het kenteken is: ".$this->kenteken."<br>";
+			$text .= "Deze".$this->merk." kwam voor het eerst in ".$this->bouwjaar." op de markt in Nederland<br>";	
+			echo $text;
+		}
 	}
 ?>
--- classes_constructor.php
+++ classes_constructor.php
@@ -1,10 +1,8 @@
 <?php
 	include_once("AutoClass.php");
 	
-	$auto1 = new AutoClass("Mercedes");
-	
-	$text = "Het merk is: ".$auto1->getMerk();
+	$auto1 = new AutoClass("Mercedes", "SLK 500", 4, "benzine", "81-UY-PZ", 2018);
 	
 ?>
 <h3>Werken met de constructor</h3>
-<?php echo $text; ?>
+<?php echo $auto1->showInfoAuto(); ?>
