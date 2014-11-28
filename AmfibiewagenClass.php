5fbe749deab1b5a89cf717fb03b1cf604527011c
Arjan de Ruijter <rra@mboutrecht.nl>
2014-11-28 09:51:18 +0100

    Overerving

--- AmfibiewagenClass.php
+++ AmfibiewagenClass.php
@@ -1,6 +1,70 @@
-Maak deze class en laat hem erven van voertuig.
-Bedenk minmaal 5 extra velden voor deze class
-Maak de properties get en set
-Maak een constructor 
-Maak een Info method
-Test deze class.
+<?php
+	require_once("./VoertuigClass.php");
+
+	class AmfibiewagenClass extends VoertuigClass
+	{
+		//Fields
+		private $kleur;
+		private $aantalZitplaatsen;
+		private $diepgang;
+		private $actieRadius;
+		private $laadVermogen;
+		
+		//Properties
+		public function getKleur() { return $this->kleur; }
+		public function getAantalZitplaatsen() { return $this->aantalZitplaatsen; }
+		public function getDiepgang() { return $this->diepgang; }
+		public function getActieRadius() { return $this->actieRadius; }
+		public function getLaadVermogen() { return $this->laadVermogen; }
+		
+		
+		public function setKleur($value) { $this->kleur = $value; }
+		public function setAantalZitplaatsen($value) { $this->aantalZitplaatsen = $value; }
+		public function setDiepgang($value) { $this->diepgang = $value; }
+		public function setActieRadius($value) { $this->actieRadius = $value; }
+		public function setLaadVermogen($value) { $this->laadVermogen = $value; }
+		
+		//Constructor
+		public function __construct($ikBenEen, $aantalWielen, $geluidsWaarschuwing,$kleur, $aantalZitplaatsen, $diepgang, $actieRadius, $laadVermogen)
+		{
+			parent::__construct($ikBenEen, $aantalWielen, $geluidsWaarschuwing);			
+			$this->kleur = $kleur;
+			$this->aantalZitplaatsen = $aantalZitplaatsen;
+			$this->diepgang = $diepgang;
+			$this->actieRadius = $actieRadius;
+			$this->laadVermogen = $laadVermogen;
+		}
+		
+		//Methods
+		public function Info()
+		{
+			parent::Info();
+			$tekst = "Het aantal zitplaatsen is: ".$this->aantalZitplaatsen."<br>";
+			$tekst .= "De diepgang is: ".$this->diepgang." meter<br>";
+			$tekst .= "De kleur is: ".$this->kleur."<br>";
+			$tekst .= "De actieRadius is: ".$this->actieRadius." km<br>";
+			$tekst .= "Het laadvermogen is: ".$this->laadVermogen." kg<br>";
+			echo $tekst;		
+		}
+	}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- inheritance_overerving.php
+++ inheritance_overerving.php
@@ -1,8 +1,16 @@
 <?php
-	include_once("CarClass.php");
+	include_once("./CarClass.php");
+	include_once("./AmfibiewagenClass.php");
+	
 	
 	$mercedes = new CarClass("Mercedes", "SLK 500S", "Diesel", "51-HB-ZP",
 								"2004", "auto", 4, "Peeeeeep");
+	
+	$peppieEnKokkie = new AmfibiewagenClass("AmfibieWagen", 4, "Blub", "Groen", 2,
+												0.8, 1400, 500);
+												
+	$peppieEnKokkie->Info();
+	echo "<br>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++<br>";
 								
 	$voertuig = new VoertuigClass("fiets", 3, "tring");
 	$voertuig->Info();
@@ -13,3 +21,4 @@
 	$test->Info();
 	
 ?>
+
