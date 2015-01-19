f7e9073645a0e38b19fe6e6c01663b5b095e1ce3
Arjan de Ruijter <rra@mboutrecht.nl>
2015-01-19 08:57:01 +0100

    Userrole geimplementeerd bij inloggen

--- checklogin.php
+++ checklogin.php
@@ -11,6 +11,27 @@
 		{
 			echo "Accountgegevens opvragen en in sessievariabelen opslaan";
 			$session->login(LoginClass::find_login_by_email_password($_POST['email'], 														  MD5($_POST['password'])));
+			
+			switch ($_SESSION['userrole'])
+			{
+				case 'customer':
+					header("location:customerHomepage.php");
+					break;
+				case 'administrator':
+					header("location:administratorHomepage.php");
+					break;
+				case 'root':
+					header("location:rootHomepage.php");
+					break;
+				case 'photographer':
+					header("location:photographerHomepage.php");
+					break;
+				case 'developer':
+					header("location:developerHomepage.php");
+					break;
+				default :
+					header("location:login_form.php");			
+			}
 		}
 		else
 		{
--- classes/SessionClass.php
+++ classes/SessionClass.php
@@ -1,11 +1,14 @@
 <?php
-	
+	session_start();
 	class SessionClass
 	{
 		//Fields
 		private $id;
 		private $email;
 		private $userrole;
+		
+		//Properties
+		public function getUserrole() { return $this->userrole; }
 	
 		//Constructor
 		public function ___construct() {}
@@ -13,6 +16,10 @@
 		public function login($loginObject)
 		{
 			// De velden $id, $email, $userrole een waarde geven.
+			//var_dump($loginObject);exit();
+			$this->id = $_SESSION['id'] = $loginObject->getId();
+			$this->email = $_SESSION['email'] = $loginObject->getEmail();
+			$this->userrole = $_SESSION['userrole'] = $loginObject->getUserrole();
 		}	
 	}
 	
