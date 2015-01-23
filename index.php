400c7f84c774abfe3718292fb504786577a2aed6
Arjan de Ruijter <rra@mboutrecht.nl>
2015-01-23 10:36:21 +0100

    CSS

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<style>
+			/*selector*/
+			section
+			/*declaration block*/
+			{
+				/*property:value;*/
+				border:20px solid red;
+				padding:0px 50px 300px 200px;
+				margin:50px;
+			}
+		
+		</style>
+	</head>
+	<body>
+		<header>
+			Dit is de kop van de webapplicatie
+		</header>
+		<aside>
+			Hier komen de hyperlinks
+		</aside>
+		<section>
+			Hier komt de inhoud van de gekozen pagina. Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen paginaHier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina		
+		</section>		
+		<footer>
+			Dit is de footer van de webapplicatie
+		</footer>	
+	</body>
+</html>
