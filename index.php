3d75c9934c481964afc319919a4294c466fec019
Arjan de Ruijter <rra@mboutrecht.nl>
2015-01-23 11:13:30 +0100

    External stylesheet

--- index.php
+++ index.php
@@ -2,25 +2,17 @@
 <html>
 	<head>
 		<title></title>
-		<style>
-			/*selector*/
-			section
-			/*declaration block*/
-			{
-				/*property:value;*/
-				border:20px solid red;
-				padding:0px 50px 300px 200px;
-				margin:50px;
-			}
-		
-		</style>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<header>
 			Dit is de kop van de webapplicatie
 		</header>
 		<aside>
-			Hier komen de hyperlinks
+			Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks.
+			Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks.Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks.
+			Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks.
+			Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks. Hier komen de hyperlinks.Hier komen de hyperlinks.
 		</aside>
 		<section>
 			Hier komt de inhoud van de gekozen pagina. Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen paginaHier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina		
