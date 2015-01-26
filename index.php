f5905905bce0fec1693e9bc3a67c7d6bc086c9c5
Arjan de Ruijter <rra@mboutrecht.nl>
2015-01-26 09:15:20 +0100

    gekke fout

--- index.php
+++ index.php
@@ -10,13 +10,24 @@
 		</header>
 		<aside>
 			<ul>
-				<li><a href=''>home</a></li>
+				<li><a href='index.php?content=algemeneHomepage'>home</a></li>
 				<li><a href=''>registreren</a></li>
 				<li><a href=''>inloggen</a></li>			
 			</ul>
 		</aside>
 		<section>
-			Hier komt de inhoud van de gekozen pagina. Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen paginaHier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina		
+			<?php
+				if ()
+				{
+				include($_GET["content"].".php");
+				}
+				else
+				{
+			?>
+			Hier komt de inhoud van de gekozen pagina. Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen paginaHier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina.Hier komt de inhoud van de gekozen pagina	
+				<?php
+				}
+				?>
 		</section>		
 		<footer>
 			Dit is de footer van de webapplicatie
