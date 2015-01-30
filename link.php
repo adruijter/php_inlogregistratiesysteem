8bc7a6e0ec871d23cc5a799e52d2681f1865c05a
Arjan de Ruijter <rra@mboutrecht.nl>
2015-01-30 09:01:04 +0100

    Link.php geschikt maken voor verschillende gebruikersrollen

--- link.php
+++ link.php
@@ -1,6 +1,15 @@
-<ul>
-	<li><a href='index.php?content=algemeneHomepage'>home</a></li>
-	<li><a href='index.php?content=register_form'>registreren</a></li>
-	<li><a href='index.php?content=Login_form'>inloggen</a></li>
-	<li><a href='index.php?content=logout'>uitloggen</a></li>
-</ul>
+<?php
+	session_start();
+	if (isset($_SESSION))
+	{
+		echo "<ul>
+				<li><a href='index.php?content=logout'>uitloggen</a></li>
+			  </ul>";	
+	}
+	echo "
+	<ul>
+		<li><a href='index.php?content=algemeneHomepage'>home</a></li>
+		<li><a href='index.php?content=register_form'>registreren</a></li>
+		<li><a href='index.php?content=Login_form'>inloggen</a></li>
+	</ul>";
+?>
