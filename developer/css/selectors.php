f75e3c25c3a032649a1705b5d9fc2788afd2c4e3
Arjan de Ruijter <rra@mboutrecht.nl>
2015-02-06 11:03:46 +0100

    float

--- developer/css/selectors.php
+++ developer/css/selectors.php
@@ -4,19 +4,36 @@
 ?>
 
 <style>
-    ul, li
-    {
-        border: 1px solid black;
-        list-style-type: none;
-    }
+ul, li
+{
+    border: 1px solid black;
+    list-style-type: none;
+    font-style
+}
+
+ul
+{
+    padding-left: 0px;
+    width: 40%;
+}
     
-    ul
-    {
-        padding-left: 0px;
-        
-    }
+ul.left
+{
+    float: left;
+}
+    
+ul.right
+{
+    float: right;
+}
+    
+div
+{
+    width: 100%;
+    border: 1px solid red;
+    clear: both;
 
-
+}
 </style>
 
 
@@ -24,8 +41,10 @@
 
 <h3>CSS selectors</h3>
 
-<h4>Boodschappenlijstje</h4>
-<ul>
+
+
+<ul class="left">
+<h4>Boodschappenlijstje 1</h4>
     <li>kaas</li>
     <li>worst</li>
     <li>Zalmsalade</li>
@@ -33,3 +52,18 @@
     <li>cola</li>
     <li>brood</li>    
 </uL>
+
+
+<ul class="right">
+<h4>Boodschappenlijstje 2</h4>
+    <li>chips</li>
+    <li>fanta</li>
+    <li>bitterballen</li>
+    <li>loempia's</li>
+    <li>drop</li>
+    <li>leverworst</li>    
+</uL>
+
+<div>
+    Dit is een div
+</div>
