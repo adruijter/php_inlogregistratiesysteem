aea118c8e299b725f845927c9612a2145f209684
Arjan de Ruijter <rra@mboutrecht.nl>
2015-05-18 13:15:52 +0200

    FadeOut

--- developer/javascript/statements.php
+++ developer/javascript/statements.php
@@ -1,39 +1,71 @@
-
-
+<style>
+    #x, #y, #uitkomst {
+        margin: 1em 0em 1em 0em;
+        padding: 0.5em;
+    }    
+    select{
+        margin: 1em 0em;
+        display:block
+    }
+</style>
 
 
 <h3>Statements</h3>
 
-getal x: <input id='x' type='text' name='x'>
+getal x: <input id='x' type='text' name='x' >
+
 <select id='operator'>
     <option value='x'>x</option>
     <option value='/'>/</option>
     <option value='+'>+</option>
     <option value='-'>-</option>
 </select>
-<br><br>
+
 getal y: <input id='y' type='text' name='y'>
 
 <button id='btn'>bereken!</button><br><br>
 
-uitkomst: <input id='uitkomst' type='text' name='y'  value='sdkfj'>
+uitkomst: <input id='result' type='text' name='y'>
+
+
 <script>
-    var x = document.getElementById('x');
-    var sign = document.getElementById('operator');
-    var y = document.getElementById('y');
+    debugger;
     var btn = document.getElementById('btn');
     btn.addEventListener('click', function(){
-        var uitkomst = document.getElementById('uitkomst');
-        var result = 0;
-        alert(sign.options[sign.selectedIndex].text);
-        if (sign.options[sign.selectedIndex].text == "/")
-        {
-            result = parseFloat(x.value) / parseFloat(y.value);
-        }
+        // Zet hier je code die na het drukken moet plaatsvinden.
+        var x = document.getElementById('x');
+        var y = document.getElementById('y');
+        var z = document.getElementById('result');
+        var operator = document.getElementById('operator');
+       
+        var choose = operator.options[operator.selectedIndex].text;
         
-        uitkomst.value = result;
-         alert(parseInt(x.value) + parseInt(y.value));
+        
+        var parseX = parseFloat(x.value);
+        var parseY = parseFloat(y.value);
+        
+        var result
+        if (choose == "x")
+        {
+            result = parseX * parseY;
+        }
+        else if( choose == "/")
+        {
+            result = parseX / parseY;
+        }
+        else if( choose == "+")
+        {
+            result = parseX + parseY;
+        }
+        else if( choose == "-")
+        {
+            result = parseX - parseY;
+        } 
+        
+        z.value = result;
+        
+        //alert("De waarde van x is: " + result);        
     });
-   
-
+    
+    
 </script>
--- developer/jquery/fade.php
+++ developer/jquery/fade.php
@@ -1,22 +1,26 @@
 <style>
- div{
+div
+{
    
 }
-#div4{
+#div4
+{
     background-color: rgb(255, 97, 97);
     width:100px;
     height:100px;
     margin:2em 0px;
 }
     
-#div5{
+#div5
+{
     background-color: rgb(243, 255, 97); 
     width:100px;
     height:100px;
     margin:2em 0px;
 }
 
-#div6{
+#div6
+{
     background-color: rgb(97, 255, 140); 
     width:100px;
     height:100px;
@@ -25,7 +29,7 @@
 </style>
 
 <h3>Fade</h3>
-<button>Klik op mij</button>
+<button>Klik op mij en het groene blokje verdwijnt!</button>
 <div id='fade'>
     <div id="div4"></div>
 
@@ -38,7 +42,7 @@
     $(document).ready(function(){
        $('button').click(function(){
            //alert("hallo");
-           $('div#fade div:first').fadeOut(3000);
+           $('div#fade div:last ').fadeOut(5000);
        });
     });
 </script>
