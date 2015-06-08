71f951d0b60a8316cd900426d936ab41392b56b3
Arjan de Ruijter <rra@mboutrecht.nl>
2015-06-08 12:51:10 +0200

    Reset Ajax

--- developer/javascript/data.php
+++ developer/javascript/data.php
@@ -1,35 +1,7 @@
 <?php
-    //require_once("http://localhost/2014-2015/fotosjaak.esy.es/config/config.php");
     // Haal gegevens uit de database en echo dit naar het scherm
-    define('SERVERNAME', 'localhost');
-        define('USERNAME', 'rra_blok1_am1a');   
-        define('PASSWORD', 'geheim');
-        define('DATABASENAME', 'blok1-am1a');
-    try
-    {
-        
-        $conn = new PDO("mysql:host=".SERVERNAME.";dbname=".DATABASENAME, USERNAME, PASSWORD);
-        
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $statement = $conn->prepare("SELECT `firstname`, `infix`, `lastname` FROM `users` WHERE  `id` = '94'");
-        $statement->execute();
-        
-        $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
-        
-        $data = "";
-        foreach($statement->fetchAll() as $key => $value)
-        {
-            $data = $value['firstname']." ".$value['infix']." ".$value['lastname'];
-        }    
-        
-    }
-    catch(PDOException $e)
-    {
-        echo "Foutmelding: ". $e->getMessage();
-    }
-
-    //$data = "sdflkj";
+   
+    $data = "Arjan de Ruijter";
     echo $data;
 ?>
 
