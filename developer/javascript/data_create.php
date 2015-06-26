9cb57253aabce0cf7614c9ae3c23c911926d8f90
Arjan de Ruijter <rra@mboutrecht.nl>
2015-06-26 11:55:45 +0200

    Laatste onderdeel create bindparam

--- developer/javascript/data_create.php
+++ developer/javascript/data_create.php
@@ -1,3 +1,31 @@
 <?php
-    echo "Het record is succesvol opgeslagen".$_POST["photo_name"].$_POST["photo_description"].$_POST["photo_path"];
+
+    $servername = "localhost";
+    $username = "rra_blok1_am1a";
+    $password = "geheim";
+    $databasename = "blok1-am1a"; 
+
+    
+    try
+    {
+        $connection = new PDO("mysql:host=".$servername.";dbname=".$databasename, $username, $password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+                
+        if (isset($_POST['photo_name']))
+        {        
+            $statement = $connection->prepare("INSERT INTO `products` (`id`, `photo_name`, `photo_description`, `photo_path`) VALUES (NULL, :photo_name, :photo_description, :photo_path)");
+            $statement->bindParam(':photo_name', $_POST['photo_name']);
+            $statement->bindParam(':photo_description', $_POST['photo_description']);
+            $statement->bindParam(':photo_path', $_POST['photo_path']);
+
+            $statement->execute();
+        }
+    }
+    catch(PDOException $e)
+    {
+        echo "Dit is een PDO foutmelding, de volgende fout heeft plaatsgevonden: ".$e->getMessage();
+    }
+  
+    echo "Het record is succesvol opgeslagen";
 ?>
